7e6f022d2fd14b179b7cb9456b27de6016aaa8d7
Andrei De Jesus <andreidejesus2021@gmail.com>
2026-06-09 15:01:41 +0800

    added a created account successfully modal and make interactive the go to dashbaord after creating a account and close btn for manually login

--- Expense Tracker/script/authentication/createAccount.js
+++ Expense Tracker/script/authentication/createAccount.js
@@ -5,6 +5,10 @@ const backBtn = document.getElementById('back-btn');
 const alreadyHaveAccountBtn = document.querySelector('.have-acc-btn');
 const emaillnput =  document.getElementById('newEmail');
 const createAccountForm = document.getElementById('create-acc-form');
+const main = document.querySelector('main');
+const accountCreatedModal = document.querySelector('.account-created');
+const closeBtn = document.querySelector('.close-btn');
+const dashboard = document.querySelector('.dashboard-btn');
 
 backBtn.addEventListener('click', () =>{
     window.location.href = 'login.html';
@@ -18,9 +22,12 @@ createAccountForm.addEventListener('submit', registerAccount);
 createAccountForm.addEventListener('keydown', (e) =>{
     if(e.target === 'Enter'){
         registerAccount(e);
+        showAccountCreated();
     }
 });
 
+let currentEmail = '';
+let loginResult = '';
 function registerAccount(e){    
     e.preventDefault();
     const firstName = document.getElementById('firstName').value.trim();
@@ -34,8 +41,30 @@ function registerAccount(e){
         return;
     }
 
+    currentEmail = email;
     userSystem.register(fullName, email, password);
+    loginResult = userSystem.checkLogin(email, password);
+    showAccountCreated();
     e.target.reset();
+}
+
+closeBtn.addEventListener('click', () => {
+    showAccountCreated();
+    return;
+});
+
+dashboard.addEventListener('click', () =>{
+    const user = loginResult.userExist;
+    localStorage.setItem('isLoggedIn', 'true');
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    localStorage.setItem('activePage', 'dashboard');
+
+    window.location.href = currentEmail === 'andreidejesus@gmail.com' ? 'adminDashboard.html' : 'userDashboard.html';
+});
+
+const showAccountCreated = () => {
+    accountCreatedModal.classList.toggle('show-account-created-modal');
+    main.classList.toggle('blur-effect');
 }
 
 console.log(userSystem.users);
